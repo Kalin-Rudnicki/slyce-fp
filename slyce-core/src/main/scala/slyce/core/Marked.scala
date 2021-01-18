2789d9999d6fe3606636259bb4d5e4e8189323b0
Kalin-Rudnicki <deaddorks@gmail.com>
2021-01-18 03:16:39 -0600

    Converted Marked from Functor to Monad. This might not really make to much sense in practice, but doing so just in case

--- slyce-core/src/main/scala/slyce/core/Marked.scala
+++ slyce-core/src/main/scala/slyce/core/Marked.scala
@@ -10,11 +10,20 @@ final case class Marked[+T](
 
 object Marked {
 
-  implicit val markedFunctor: Functor[Marked] =
-    new Functor[Marked] {
+  implicit val markedMonad: Monad[Marked] =
+    new Monad[Marked] {
 
       override def map[A, B](t: Marked[A], f: A => B): Marked[B] =
         Marked(f(t.value), t.span)
+
+      override def apply[A, B](t: Marked[A], f: Marked[A => B]): Marked[B] =
+        Marked(f.value(t.value), Span.join(t.span, f.span))
+
+      override def pure[A](a: A): Marked[A] =
+        Marked(a)
+
+      override def flatten[A](t: Marked[Marked[A]]): Marked[A] =
+        Marked(t.value.value, Span.join(t.span, t.value.span))
 
     }
 
