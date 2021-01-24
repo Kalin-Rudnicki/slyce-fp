4431f00f1ab55099b6adfb2bd40ae38185d9212c
Kalin-Rudnicki <deaddorks@gmail.com>
2021-01-24 17:10:30 -0600

    Starting Grammar/AugmentedGrammar

---
+++ slyce-generate/src/main/scala/slyce/generate/IgnoredList.scala
@@ -0,0 +1,86 @@
+package slyce.generate
+
+import klib.Implicits._
+import klib.fp.typeclass._
+import klib.fp.types._
+
+import scala.annotation.tailrec
+
+final case class IgnoredList[+T](
+    before: List[T],
+    unIgnored: T,
+    after: List[T],
+) {
+
+  def unIgnoredIdx: Int =
+    before.size
+
+  def toList: List[T] =
+    before ::: unIgnored :: after
+
+}
+
+object IgnoredList {
+
+  def fromList[T](list: List[(Boolean, T)]): Maybe[IgnoredList[T]] = {
+    @tailrec
+    def ensure(
+        queue: List[(Boolean, T)],
+        il: IgnoredList[T],
+    ): Maybe[IgnoredList[T]] =
+      queue match {
+        case (unIgn, _) :: tail =>
+          if (unIgn)
+            None
+          else
+            ensure(tail, il)
+        case Nil =>
+          il.some
+      }
+
+    @tailrec
+    def find(
+        queue: List[(Boolean, T)],
+        stack: List[T],
+    ): Maybe[IgnoredList[T]] =
+      queue match {
+        case (unIgn, t) :: tail =>
+          if (unIgn)
+            ensure(
+              tail,
+              IgnoredList(stack.reverse, t, tail.map(_._2)),
+            )
+          else
+            find(
+              tail,
+              t :: stack,
+            )
+        case Nil =>
+          None
+      }
+
+    list match {
+      case (_, t) :: Nil =>
+        IgnoredList(
+          before = Nil,
+          unIgnored = t,
+          after = Nil,
+        ).some
+      case _ =>
+        find(list, Nil)
+    }
+  }
+
+  implicit val ignoredListFunctor: Functor[IgnoredList] =
+    new Functor[IgnoredList] {
+
+      override def map[A, B](t: IgnoredList[A], f: A => B): IgnoredList[B] =
+        IgnoredList(
+          before = t.before.map(f),
+          unIgnored = f(t.unIgnored),
+          after = t.after.map(f),
+        )
+
+    }
+
+}
--- slyce-generate/src/main/scala/slyce/generate/input/Grammar.scala
+++ slyce-generate/src/main/scala/slyce/generate/input/Grammar.scala
@@ -7,17 +7,58 @@ import slyce.generate._
 
 final case class Grammar(
     startNt: Marked[String],
+    nts: List[Grammar.NT],
 )
 
 object Grammar {
 
   final case class NT(
       name: Marked[String],
+      nt: NonTerminal,
   )
 
-  sealed trait Element
   sealed trait NonTerminal
+  sealed trait Element
 
-  final case class Identifier(name: Marked[String]) extends Element
+  sealed trait StandardNonTerminal
+  object StandardNonTerminal {
+
+    final case class `:`(
+        reductions: NonEmptyList[List[Element]],
+    ) extends StandardNonTerminal
+
+    final case class ^(
+        reductions: NonEmptyList[IgnoredList[Element]],
+    ) extends StandardNonTerminal
+
+  }
+
+  final case class ListNonTerminal(
+      `type`: ListNonTerminal.Type,
+      start: IgnoredList[Element],
+      repeat: Maybe[IgnoredList[Element]],
+  ) extends NonTerminal
+      with Element
+
+  object ListNonTerminal {
+    sealed trait Type
+    object Type {
+      case object + extends Type
+      case object * extends Type
+    }
+  }
+
+  final case class AssocNonTerminal(
+      assoc: NonEmptyList[(AssocNonTerminal.Type, Element)],
+      base: StandardNonTerminal,
+  ) extends NonTerminal
+
+  object AssocNonTerminal {
+    sealed trait Type
+    object Type {
+      case object Left extends Type
+      case object Right extends Type
+    }
+  }
 
 }
