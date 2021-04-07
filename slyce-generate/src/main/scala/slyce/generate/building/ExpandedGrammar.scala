787b0d78ed6586992a2c4cec448f0b82d0a1401e
Kalin-Rudnicki <deaddorks@gmail.com>
2021-04-06 20:19:36 -0500

    minor progress with ExpandedGrammar

--- slyce-generate/src/main/scala/slyce/generate/building/ExpandedGrammar.scala
+++ slyce-generate/src/main/scala/slyce/generate/building/ExpandedGrammar.scala
@@ -1,28 +1,31 @@
 package slyce.generate.building
 
-import scala.annotation.tailrec
-
 import klib.Implicits._
 import klib.fp.types._
 import slyce.core._
 import slyce.generate._
 import slyce.generate.input.Grammar
-import slyce.generate.input.Grammar.StandardNonTerminal
 
 final case class ExpandedGrammar private (
     startNt: Marked[String], // TODO (KR) : Remove?
-    // TODO (KR) :
+    nts: List[ExpandedGrammar.NT],
+    aliases: ExpandedGrammar.AliasMap,
 )
 
 object ExpandedGrammar {
 
+  type AliasMap = Map[ExpandedGrammar.Identifier.NonTerminal, ExpandedGrammar.Identifier.NonTerminal]
+
   /*
     NOTE (KR) : Types of identifiers
-              : - Normal -> `Name`
-              : - ListNt -> ListHead__`Name`, ListTail__`Name`
-              : - AnonListNt -> AnonListHead__`Num`, AnonListTail__`Num`
-              : - AnonOptNt -> AnonOpt__`Name`
-              : - AssocNt
+              : `GINT` =  Whatever the Grammar.Identifier.NonTerminal was
+              : `UUID` = A sequential int used to differentiate anonymously generated things
+              :
+              : - Normal -> `GINT`
+              : - ListNt -> List[/Head/Tail]__`GINT`
+              : - AnonListNt -> AnonList[/Head/Tail]__`UUID`
+              : - AnonOptNt -> AnonOpt__`todo`
+              : - AssocNt -> Assoc__`GINT`__`idx`
 
     NOTE (KR) : Problems that need to be addressed:
               : - Would be ideal to have accurate names
@@ -45,39 +48,61 @@ object ExpandedGrammar {
         case object Tail extends ListType
       }
 
+      final class Key
+
       final case class NormalNt(name: String) extends NonTerminal
       final case class ListNt(name: String, `type`: ListType) extends NonTerminal
-      final case class AnonListNt(id: Int, `type`: ListType) extends NonTerminal
+      final case class AnonListNt(key: Key, `type`: ListType) extends NonTerminal
       final case class AssocNt(name: String, idx: Int) extends NonTerminal
+      // TODO (KR) : Maybe use Grammar.Identifier instead, which already handles differentiating this?
+      //           : But other places here dont use it...
+      //           : Should they all? Should none of them? Is just 1 ok?
+      final case class AnonOptNt(name: String, `type`: AnonOptNt.Type) extends NonTerminal
+      object AnonOptNt {
+        sealed trait Type
+        object Type {
+          case object NonTerminal extends Type
+          case object Terminal extends Type
+          case object Raw extends Type
+        }
+      }
     }
 
-    final case class Terminal(name: String, raw: Boolean) extends Identifier
+    final case class Terminal(name: String) extends Identifier
+    final case class Raw(name: String) extends Identifier
+  }
+
+  final case class NT(
+      name: Identifier.NonTerminal,
+      reductions: NonEmptyList[NT.Reduction],
+  )
+
+  object NT {
+    final case class Reduction(elements: List[Identifier])
   }
 
   // =====|  |=====
 
   def fromGrammar(grammar: Grammar): Attempt[ExpandedGrammar] = {
-    def handleNonTerminal(nonTerminal: Grammar.NonTerminal): Nothing =
-      nonTerminal match {
-        case terminal: StandardNonTerminal =>
-          terminal match {
-            case StandardNonTerminal.`:`(reductions) =>
-              // TODO (KR) :
-              ???
-            case StandardNonTerminal.^(reductions) =>
-              // TODO (KR) :
-              ???
-          }
-        case Grammar.ListNonTerminal(_type, start, repeat) =>
-          // TODO (KR) :
-          ???
-        case Grammar.AssocNonTerminal(assocs, base) =>
-          // TODO (KR) :
-          ???
-      }
+    final case class Expansion(
+        identifier: Identifier,
+        generatedNts: List[NT],
+        aliases: AliasMap,
+    )
+
+    def expandNonTerminal(nonTerminal: Grammar.NonTerminal): Attempt[Expansion] = {
+      // TODO (KR) :
+      ???
+    }
+
+    def expandElement(element: Marked[Grammar.Element]): Attempt[Expansion] = {
+
+      // TODO (KR) :
+      ???
+    }
 
     // TODO (KR) :
-    ???
+    Alive(null)
   }
 
 }
