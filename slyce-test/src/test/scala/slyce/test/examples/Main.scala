d192a67fa11c435acf7e6c45100f2a23a9db523b
Kalin-Rudnicki <kalin.rudnicki@gmail.com>
2021-07-09 16:34:30 -0500

    Migrated to new Executable paradigm

--- slyce-test/src/test/scala/slyce/test/examples/Main.scala
+++ slyce-test/src/test/scala/slyce/test/examples/Main.scala
@@ -8,30 +8,31 @@ import slyce.core._
 
 object Main {
 
-  val test: Executable = { (logger, _) =>
-    for {
-      _ <- logger(L.log.info("--- Testing ---"))
+  val test: Executable =
+    Executable { (logger, _) =>
+      for {
+        _ <- logger(L.log.info("--- Testing ---"))
 
-      src = Source(0.until(5).map(_ => "1234_6789").mkString("\n"))
-      _ <- logger(L.log.info("Source:"))
-      _ <- logger(L.log.info(src.input))
+        src = Source(0.until(5).map(_ => "1234_6789").mkString("\n"))
+        _ <- logger(L.log.info("Source:"))
+        _ <- logger(L.log.info(src.input))
 
-      marks = List(
-        Marked("Start", Span(Span.Pos.Start, Span.Pos.Start).some),
-        Marked("Rest of\n(start)", Span(Span.Pos(2, 1, 2), Span.Pos(4, 1, 4)).some),
-        Marked("Should end up as an EOF", Span(Span.Pos(3, 1, 3), Span.Pos(3, 1, 3)).some),
-        Marked("(1) 2 messages in same spot", Span(Span.Pos(16, 2, 6), Span.Pos(19, 2, 9)).some),
-        Marked("(2) 2 messages in same spot", Span(Span.Pos(16, 2, 6), Span.Pos(19, 2, 9)).some),
-        Marked("(3) Actually,\nhere's a 3rd", Span(Span.Pos(16, 2, 6), Span.Pos(19, 2, 9)).some),
-        //
-        Marked("EOF Message 1"),
-        Marked("EOF Message 2"),
-        Marked("EOF Message 3\nAnd the second line too"),
-      )
-      _ <- logger(L.log.info("Source (Marked):"))
-      _ <- logger(L.log.info(src.mark(marks)))
-    } yield ()
-  }
+        marks = List(
+          Marked("Start", Span(Span.Pos.Start, Span.Pos.Start).some),
+          Marked("Rest of\n(start)", Span(Span.Pos(2, 1, 2), Span.Pos(4, 1, 4)).some),
+          Marked("Should end up as an EOF", Span(Span.Pos(3, 1, 3), Span.Pos(3, 1, 3)).some),
+          Marked("(1) 2 messages in same spot", Span(Span.Pos(16, 2, 6), Span.Pos(19, 2, 9)).some),
+          Marked("(2) 2 messages in same spot", Span(Span.Pos(16, 2, 6), Span.Pos(19, 2, 9)).some),
+          Marked("(3) Actually,\nhere's a 3rd", Span(Span.Pos(16, 2, 6), Span.Pos(19, 2, 9)).some),
+          //
+          Marked("EOF Message 1"),
+          Marked("EOF Message 2"),
+          Marked("EOF Message 3\nAnd the second line too"),
+        )
+        _ <- logger(L.log.info("Source (Marked):"))
+        _ <- logger(L.log.info(src.mark(marks)))
+      } yield ()
+    }
 
   def main(args: Array[String]): Unit = {
     Executable
--- slyce-test/src/test/scala/slyce/test/examples/package.scala
+++ slyce-test/src/test/scala/slyce/test/examples/package.scala
@@ -2,6 +2,8 @@ package slyce.test
 
 import java.io.File
 
+import org.rogach.scallop._
+
 import klib.Implicits._
 import klib.fp.types._
 import klib.utils._
@@ -13,53 +15,59 @@ import slyce.generate.main._
 
 package object examples {
 
+  final class GenerateConf(args: Seq[String]) extends Executable.Conf(args) {
+    val debugOutput: ScallopOption[Boolean] = opt()
+    val fileOutput: ScallopOption[Boolean] = opt()
+
+    verify()
+  }
+  object GenerateConf extends Executable.ConfBuilder(new GenerateConf(_))
+
   def debugGenerate(
       name: String,
       lexer: Lexer,
       grammar: Grammar,
-  ): Executable = { (logger, args) =>
-    val debugOutput = args.contains("-D")
-    val fileOutput = args.contains("-F")
-
-    for {
-      _ <- logger(
-        L(
-          L.ansi.cursorPos(1, 1),
-          L.ansi.clearScreen(),
-          L.log.important(s"=====| $name |====="),
-          L.break(),
-          L.log.info("Building..."),
-        ),
-      )
-      buildInput = BuildInput(name, lexer, grammar)
-      aBuildResult = Build.buildOutput(buildInput)
-      _ <-
-        if (debugOutput)
-          for {
-            _ <- logger(L.log.info("Writing DebugOutput..."))
-            _ <- OutputDebug.outputDebug(buildInput, aBuildResult)
-          } yield ()
-        else
-          ().pure[IO]
-      _ <-
-        if (fileOutput)
-          aBuildResult match {
-            case Alive(buildResult) =>
-              for {
-                _ <- logger(L.log.info("Writing FileOutput..."))
-                pkg = List("slyce", "test", "examples", name)
-                outputIdtStr = Build.outputToString(pkg, buildResult)
-                outputStr = outputIdtStr.toString("  ")
-                outputFile = new File(List(List("slyce-test", "src", "test", "scala"), pkg, List(s"$name.scala")).flatten.mkString("/"))
-                _ <- IO.writeFile(outputFile, outputStr)
-              } yield ()
-            case Dead(_) =>
-              ().pure[IO]
-          }
-        else
-          ().pure[IO]
-      _ <- logger(L.log.info("Done."))
-    } yield ()
-  }
+  ): Executable =
+    Executable.fromConf(GenerateConf) { (logger, conf) =>
+      for {
+        _ <- logger(
+          L(
+            L.ansi.cursorPos(1, 1),
+            L.ansi.clearScreen(),
+            L.log.important(s"=====| $name |====="),
+            L.break(),
+            L.log.info("Building..."),
+          ),
+        )
+        buildInput = BuildInput(name, lexer, grammar)
+        aBuildResult = Build.buildOutput(buildInput)
+        _ <-
+          if (conf.debugOutput())
+            for {
+              _ <- logger(L.log.info("Writing DebugOutput..."))
+              _ <- OutputDebug.outputDebug(buildInput, aBuildResult)
+            } yield ()
+          else
+            ().pure[IO]
+        _ <-
+          if (conf.fileOutput())
+            aBuildResult match {
+              case Alive(buildResult) =>
+                for {
+                  _ <- logger(L.log.info("Writing FileOutput..."))
+                  pkg = List("slyce", "test", "examples", name)
+                  outputIdtStr = Build.outputToString(pkg, buildResult)
+                  outputStr = outputIdtStr.toString("  ")
+                  outputFile = new File(List(List("slyce-test", "src", "test", "scala"), pkg, List(s"$name.scala")).flatten.mkString("/"))
+                  _ <- IO.writeFile(outputFile, outputStr)
+                } yield ()
+              case Dead(_) =>
+                ().pure[IO]
+            }
+          else
+            ().pure[IO]
+        _ <- logger(L.log.info("Done."))
+      } yield ()
+    }
 
 }
